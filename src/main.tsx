d1a2489da059494633a9f13c56d9d01620762586
Vladimir Kutsev <vladimirkucev6@gmail.com>
2025-04-07 12:40:38 +0300

    router + mocks in place

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Home from './pages/Home.tsx'
+import NotFound from './pages/NotFound.tsx'
+import User from './pages/User.tsx'
+
+const router = createBrowserRouter([
+	{ path: '/', element: <Home /> },
+	{ path: '/users/:id', element: <User /> },
+	{ path: "*", element: <NotFound /> }
+])
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
 )
